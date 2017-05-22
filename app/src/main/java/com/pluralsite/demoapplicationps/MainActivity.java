e2d1f6637be614411a5fc77213fb8d505c6a634a
rmanaugmedix <arman@augmedix.com>
2017-05-22 11:11:01 +0600

    Update

--- app/src/main/java/com/pluralsite/demoapplicationps/MainActivity.java
+++ app/src/main/java/com/pluralsite/demoapplicationps/MainActivity.java
@@ -14,10 +14,12 @@ import android.text.style.RelativeSizeSpan;
 import android.text.style.StrikethroughSpan;
 import android.text.style.StyleSpan;
 import android.text.style.TextAppearanceSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE;
 
@@ -32,30 +34,36 @@ public class MainActivity extends Activity {
 
         context = getApplicationContext();
         button1 = (Button) findViewById(R.id.button);
-        TextView TV = (TextView)findViewById(R.id.header);
+        TextView tv1 = (TextView)findViewById(R.id.header);
+        TextView tv2 = (TextView)findViewById(R.id.textView2);
 
         String[] a = new String[2];
         a[0] = "test1";
         a[1] = "test2";
 
-        Spannable wordtoSpan = new SpannableString(getResources().getString(R.string.retain_talent));
+        Spannable wordtoSpan1 = new SpannableString(getResources().getString(R.string.retain_talent));
+        Spannable wordtoSpan2 = new SpannableString(getResources().getString(R.string.do_not_have_an_account));
 
-        wordtoSpan.setSpan(new StyleSpan(Typeface.BOLD),7,12, SPAN_EXCLUSIVE_EXCLUSIVE);
-        wordtoSpan.setSpan(new ForegroundColorSpan(Color.GRAY), 0, 6, SPAN_EXCLUSIVE_EXCLUSIVE);
-        wordtoSpan.setSpan(new RelativeSizeSpan(0.75f), 7,13, 0);
-        wordtoSpan.setSpan(new StrikethroughSpan(), 0,6, 0);
-        wordtoSpan.setSpan(new TextAppearanceSpan(context, R.style.CustomTextStyle),7,13, SPAN_EXCLUSIVE_EXCLUSIVE);
+        wordtoSpan1.setSpan(new StyleSpan(Typeface.BOLD),7,12, SPAN_EXCLUSIVE_EXCLUSIVE);
+        wordtoSpan1.setSpan(new ForegroundColorSpan(Color.GRAY), 0, 6, SPAN_EXCLUSIVE_EXCLUSIVE);
+        wordtoSpan1.setSpan(new RelativeSizeSpan(0.75f), 7,13, 0);
+        wordtoSpan1.setSpan(new StrikethroughSpan(), 0,6, 0);
+        wordtoSpan1.setSpan(new UnderlineSpan(), 7,13, 0);
+
+        wordtoSpan2.setSpan(new TextAppearanceSpan(context, R.style.CustomTextStyle),0,wordtoSpan2.length(), SPAN_EXCLUSIVE_EXCLUSIVE);
 
         //wordtoSpan.setSpan(new SuggestionSpan(context, a, SuggestionSpan.FLAG_AUTO_CORRECTION),7,12,0);
-        wordtoSpan.setSpan(new ClickableSpan() {
+        wordtoSpan2.setSpan(new ClickableSpan() {
             @Override
             public void onClick(View view) {
-                Log.d("Testing", "Tested");
+                Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show();
             }
-        },0,6,0);
+        },wordtoSpan2.length()-11,wordtoSpan2.length(),0);
 
-        TV.setText(wordtoSpan);
-        TV.setMovementMethod(LinkMovementMethod.getInstance());
+        tv1.setText(wordtoSpan1);
+
+        tv2.setText(wordtoSpan2);
+        tv2.setMovementMethod(LinkMovementMethod.getInstance());
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
